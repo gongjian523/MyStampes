8049f64d0408224746301a1aabb52a2f1ae12abf
gongjian <jian.gong@istuary.com>
2016-12-20 17:14:38 +0800

    add

--- MyStampes/App.xaml.cs
+++ MyStampes/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyStampes.SQLiteHerlper;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,7 +14,11 @@ namespace MyStampes
     /// </summary>
     public partial class App : Application
     {
-    }
 
-    
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            SQLiteHelper.Instance.InitDB();
+        }
+    }
 }
