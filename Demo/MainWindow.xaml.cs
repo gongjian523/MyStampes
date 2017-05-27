d7d17674f0267bb43dbf62caa0a1cffc2e77fb8f
gongjian <jian.gong@istuary.com>
2017-05-27 10:41:32 +0800

    commit

---
+++ Demo/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private View1 view1 = new View1();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ContentFrame.Navigate(view1);
+        }
+
+        private void EnterView1(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(view1);
+        }
+        
+    }
+}
