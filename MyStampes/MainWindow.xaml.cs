5ce42f554ff24a1114a3271e72fc5ef354dd4b29
gongjian <jian.gong@istuary.com>
2017-06-09 14:33:40 +0800

    update

--- MyStampes/MainWindow.xaml.cs
+++ MyStampes/MainWindow.xaml.cs
@@ -16,6 +16,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MyStampes.AddrBook;
 using MyStampes.Log;
+using System.Windows.Interop;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace MyStampes
 {
@@ -43,6 +46,56 @@ namespace MyStampes
         {
             ContentFrame.Navigate(addrBookView);
         }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MaximizedWindow(object sender, RoutedEventArgs e)
+        {
+            //if (this.WindowState == WindowState.Maximized)
+            //    this.WindowState = WindowState.Normal;
+            //else if (this.WindowState == WindowState.Normal)
+
+
+            var handle = new WindowInteropHelper(this).Handle;
+
+            Screen screen = Screen.FromHandle(handle);
+
+            this.MaxWidth = screen.Bounds.Width;
+            this.MaxHeight = screen.Bounds.Height;
+            this.WindowState = WindowState.Maximized;
+
+            RestoreDownBtn.Visibility = System.Windows.Visibility.Visible;
+            MaximizedBtn.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+             this.WindowState = WindowState.Minimized;
+        }
+
+        private void RestoreDownWindow(object sender, RoutedEventArgs e)
+        {
+            //if (this.WindowState == WindowState.Minimized)
+            //    this.WindowState = WindowState.Normal;
+            //else if (this.WindowState == WindowState.Normal)
+            this.WindowState = WindowState.Normal;
+
+            MaximizedBtn.Visibility = System.Windows.Visibility.Visible;
+            RestoreDownBtn.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void DragMove(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == System.Windows.WindowState.Maximized)
+                this.WindowState = System.Windows.WindowState.Normal;
+
+            this.DragMove();
+        }
+
+
     }
 
 }
