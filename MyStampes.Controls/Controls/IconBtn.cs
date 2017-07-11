c9acc62b22f1daba602995b5156b477706f4e3ce
gongjian <jian.gong@istuary.com>
2017-07-11 14:14:33 +0800

    update

--- MyStampes.Controls/Controls/IconBtn.cs
+++ MyStampes.Controls/Controls/IconBtn.cs
@@ -5,23 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace MyStampes.Controls.Controls
 {
     public class IconBtn : Button
     {
-        //public IconBtn()
-        //{
-        //}
 
-        public string NomalPic
+        public string NormalPic
         {
-            get { return (string)GetValue(NomalPicProperty); }
-            set { SetValue(NomalPicProperty, value); }
+            get { return (string)GetValue(NormalPicProperty); }
+            set { SetValue(NormalPicProperty, value); }
         }
 
-        public static readonly DependencyProperty NomalPicProperty =
-            DependencyProperty.Register("NomalPic", typeof(string), typeof(IconBtn), new PropertyMetadata(""));
+        public static readonly DependencyProperty NormalPicProperty =
+            DependencyProperty.Register("NormalPic", typeof(string), typeof(IconBtn), new PropertyMetadata(""));
 
 
         public string MouseOnPic
@@ -32,6 +30,18 @@ namespace MyStampes.Controls.Controls
 
         public static readonly DependencyProperty MouseOnPicProperty =
             DependencyProperty.Register("MouseOnPic", typeof(string), typeof(IconBtn), new PropertyMetadata(""));
+
+
+
+        public Canvas IconCanvas
+        {
+            get { return (Canvas)GetValue(IconCanvasProperty); }
+            set { SetValue(IconCanvasProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconCanvasProperty =
+            DependencyProperty.Register("IconCanvas", typeof(Canvas), typeof(IconBtn));
+
 
     }
 }
